e9de59040a51141e2ff95b5c6fcad8395b44296b
Seth Bushinsky <smb4@hawaii.edu>
2026-09-02 13:40:48 -1000

    replacing website with fresh start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,49 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Hi-Cycles',
-      social: [
-        { icon: 'github', label: 'GitHub', href: 'https://github.com/Hi-Cycles' },
-      ],
-      sidebar: [],
-    }),
-  ],
+	vite: {
+		server: {
+			watch: {
+				usePolling: true,
+				interval: 100,
+				binaryInterval: 300,
+				ignored: ['**/.git/**', '**/.dropbox**', '**/.DS_Store', '**/node_modules/**'],
+				awaitWriteFinish: {
+					stabilityThreshold: 100,
+					pollInterval: 100,
+				},
+			},
+		},
+	},
+	integrations: [
+		starlight({
+			title: 'HI-Cycles',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Hi-Cycles/' }],
+			sidebar: [
+				{
+					label: 'Research',
+					items: [{ label: 'Projects', slug: 'research' },
+						 { label: 'Publications', slug: 'publications' }],
+				},
+				// {
+				// 	label: 'Publications',
+				// 	items: [{ label: 'Publications', slug: 'publications' }],
+				// },
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	items: [{ autogenerate: { directory: 'reference' } }],
+				// },
+			],
+		}),
+	],
 });
---
+++ src/content/docs/Research.md
@@ -0,0 +1,16 @@
+---
+title: Research Projects
+description: An overview of the topics we work on.
+---
+
+# Testing 
+
+Blah blah
+
+## test test
+
+blah more detail blah
+
+# testing 
+
+### test test 
